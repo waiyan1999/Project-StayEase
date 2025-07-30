aeab797fe52f20b548309d1b888fb1822b738aff
waiyan <wiayan1661999@gmai.com>
2025-07-30 21:47:18 +0630

    allow host updated

--- stayEase/stayEase/settings.py
+++ stayEase/stayEase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7rq^%p2wyxkkw-8$eqrur4yd&#8qnsbi2z%znf9w507#0@qlqn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
